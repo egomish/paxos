ec8aaa776256852016efe38ba7f264cd880cedb2
Emma Gomish <egomish@ucsc.edu>
2018-10-26 12:50:14 -0700

    uncommented tests and added some new tests in LocalTester

--- LocalTester.java
+++ LocalTester.java
@@ -6,14 +6,12 @@ main (String[] args)
 {
     HttpResponse response;
 
-/*
 //hello world
     System.out.println("hello world: ");
     response = ClientRequest.sendRequest("localhost:8080",
                                         "GET", "/hello", null, 
                                         null);
     System.out.println("8080: " + response.getResponseBody());
-*/
 
 //put new key "foo"="bart"
     System.out.println("foo=bart: ");
@@ -21,18 +19,31 @@ main (String[] args)
     System.out.println("service: " + service);
     response = ClientRequest.sendRequest("localhost:8080",
                                          "PUT", service, null, 
-                                         "{'val':'bart bart bart'}");
+                                         "{'val':'bart'}");
     System.out.println("8080: " + response.getResponseBody());
-    System.exit(5);
 
-/*
+//put new key "subject"="Distributed System"
+    System.out.println("subject=Distributed System: ");
+    response = ClientRequest.sendRequest("localhost:8080",
+                                         "PUT", "/keyValue-store/subject", 
+                                         null,
+                                         "{'val': 'Distributed System'}");
+    System.out.println("8080: " + response.getResponseBody());
+
+//get key "subject"
+    System.out.println("subject=?: ");
+    response = ClientRequest.sendRequest("localhost:8080",
+                                         "GET", "/keyValue-store/subject", 
+                                         null,
+                                         null);
+    System.out.println("8080: " + response.getResponseBody());
+
 //check for key "foo"
     System.out.println("foo?: ");
     response = ClientRequest.sendRequest("localhost:8080",
                                          "GET", "/keyValue-store/search/foo", null,
                                          null);
     System.out.println("8080: " + response.getResponseBody());
-*/
 
 //get key "foo"
     System.out.println("foo=?: ");
